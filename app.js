e9a0329b26bcb83529e15325c2b8309f8d846d15
sd <sd@gmail.com>
2026-07-28 03:40:53 +0530

    Updated dashboard page

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+// Serve all files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default route
 app.get('/', (req, res) => {
-    res.send('DevOps CI/CD Project Working');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Running on port 3000');
-});
+const PORT = 3000;
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
